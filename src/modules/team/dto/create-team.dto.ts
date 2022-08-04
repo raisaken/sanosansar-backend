638ec37d86ee5452dc5780c82fcea7f63e1b348e
mondyfy <khaptadisandip@gmail.com>
2022-08-05 00:22:39 +0545

    update team validation

--- src/modules/team/dto/create-team.dto.ts
+++ src/modules/team/dto/create-team.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional } from "class-validator";
+import { IsOptional, MaxLength } from "class-validator";
 
 export class CreateTeamDto {
+    @MaxLength(50)
     @ApiProperty({ type: String })
     name: string;
     
     @ApiProperty({ type: String })
     description?: string;
     
+    @MaxLength(50)
     @ApiProperty({ type: String })
     position?: string;
     
@@ -17,6 +19,8 @@ export class CreateTeamDto {
     @ApiProperty()
     phone?: number;
     
+    @MaxLength(30)
+    @IsOptional()
     @ApiProperty({ type: String })
     email?: string;
     
