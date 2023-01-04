53c08ba7294b0b71af2dc31dc7c62ca4d4ee5a88
mondyfy <khaptadisandip@gmail.com>
2023-01-04 11:14:46 +0545

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ import * as basicAuth from 'express-basic-auth';
 async function bootstrap() {
   const swaggerEnvs = ['local', 'dev', 'staging'];
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // app.enableCors();
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const prefix: string = process.env.API_GLOBAL_PREFIX ?? 'api';
   const version: string = process.env.API_GLOBAL_VERSION ?? 'v1';
@@ -48,6 +47,16 @@ async function bootstrap() {
       document,
     );
   }
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+  app.enableCors({
+    allowedHeaders: "*",
+    origin: "*"
+  });
   await app.listen(port);
   Logger.log(`Application started on port: ${port}`);
 }
